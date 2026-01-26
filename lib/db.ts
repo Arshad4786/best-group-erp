3a5c793ad4026a04d08ea65598ae04ea6fbed9cf
Arshad4786 <arshad88908@gmail.com>
2026-01-27 04:12:05 +0530

    limit on max clients

--- lib/db.ts
+++ lib/db.ts
@@ -2,21 +2,37 @@ import { PrismaClient } from "@prisma/client";
 import { Pool } from "pg";
 import { PrismaPg } from "@prisma/adapter-pg";
 
-const connectionString = `${process.env.DATABASE_URL}`;
-
-const pool = new Pool({ connectionString });
-const adapter = new PrismaPg(pool);
-
-const prismaClientSingleton = () => {
-  return new PrismaClient({ adapter });
-};
+const connectionString = process.env.DATABASE_URL!;
 
 declare global {
-  var prisma: undefined | ReturnType<typeof prismaClientSingleton>;
+  // eslint-disable-next-line no-var
+  var prisma: PrismaClient | undefined;
+  // eslint-disable-next-line no-var
+  var pgPool: Pool | undefined;
 }
 
-const prisma = globalThis.prisma ?? prismaClientSingleton();
+const pool =
+  globalThis.pgPool ??
+  new Pool({
+    connectionString,
+    max: 5, 
+  });
+
+if (process.env.NODE_ENV !== "production") {
+  globalThis.pgPool = pool;
+}
+
+const adapter = new PrismaPg(pool);
+
+const prisma =
+  globalThis.prisma ??
+  new PrismaClient({
+    adapter,
+    log: ["error"],
+  });
+
+if (process.env.NODE_ENV !== "production") {
+  globalThis.prisma = prisma;
+}
 
 export default prisma;
-
-if (process.env.NODE_ENV !== "production") globalThis.prisma = prisma;
